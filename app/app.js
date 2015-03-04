9fc332af256e92519eee6feef85c91c3a0859ef4
Marcin Grzywaczewski <killavus@gmail.com>
2015-03-04 13:12:54 +0100

    DI configured by a config - example

--- app/app.js
+++ app/app.js
@@ -1,12 +1,49 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var AngularApp, ImplementationA, ImplementationB, Dependency;
+
+Dependency = function() {};
+Dependency.prototype.magicNumber = 42;
+
+ImplementationA = function(dependency) {
+  this.dependency = dependency;
+};
+
+ImplementationA.prototype.foo = function() {
+  return "bar" + this.dependency.magicNumber;
+};
+
+ImplementationB = function(dependency) {
+  this.dependency = dependency;
+};
+
+ImplementationB.prototype.foo = function() {
+  return "baz" + this.dependency.magicNumber;
+}
+
+AngularApp = angular.module('diFunApp', [])
+        // Config:
+        // Change a constant underneath to change underlying implementation of ServiceWithConfiguredImplementation:
+        .constant("EnvType", "test")
+        // Registering implementations:
+        .constant("ImplementationA", ImplementationA)
+        .constant("ImplementationB", ImplementationB) 
+        // Constant implementation:
+        .service('Dependency', Dependency);
+
+AngularApp.provider('ServiceWithConfiguredImplementation', ['EnvType', 'ImplementationA', 'ImplementationB', function(env, implementationA, implementationB) {
+  var implementation = {
+    true: implementationA,
+    false: implementationB
+  };
+
+  this.$get = ['Dependency', 'EnvType', function(Dependency, EnvType) {
+    return new implementation[EnvType == "test"](Dependency);
+  }];
+
+  return this;
+}]);
+
+AngularApp.controller('Controller', ['$scope', 'ServiceWithConfiguredImplementation', function($scope, service) {
+  $scope.value = service.foo();
 }]);
